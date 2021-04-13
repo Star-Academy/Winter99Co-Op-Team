f9391d8c52b20fdd84868eef7ce55b1b7b1966da
hoseinaghaei <59363901+hoseinaghaei@users.noreply.github.com>
2021-04-13 10:04:13 +0430

    finish Importer.cs.

--- Winter99Co-Op-Team/elasticsearch/SearchConnection/Importer.cs
+++ Winter99Co-Op-Team/elasticsearch/SearchConnection/Importer.cs
@@ -1,7 +1,34 @@
-﻿namespace Winter99Co_Op_Team.elasticsearch.SearchConnection
+﻿using System.Collections.Generic;
+using Nest;
+
+namespace Winter99Co_Op_Team.elasticsearch.SearchConnection
 {
     public class Importer<T> where T : class
     {
-        
+        private readonly IElasticClient _elasticClient;
+
+        public Importer(IElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
+
+        public void Import(IEnumerable<T> documents, string indexName)
+        {
+            var bulk = SetupBulk(documents, indexName);
+            _elasticClient.Bulk(bulk);
+        }
+
+        private static BulkDescriptor SetupBulk(IEnumerable<T> documents, string indexName)
+        {
+            var bulk = new BulkDescriptor();
+            foreach (var document in documents)
+            {
+                bulk.Index<T>(x => x
+                    .Index(indexName)
+                    .Document(document));
+            }
+
+            return bulk;
+        }
     }
 }
