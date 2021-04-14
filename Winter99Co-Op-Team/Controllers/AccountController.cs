fa6b42a7c8cad11b59b109ebfadfa42322af5848
hoseinaghaei <59363901+hoseinaghaei@users.noreply.github.com>
2021-04-14 11:56:23 +0430

    Finish Controller classes.

---
+++ Winter99Co-Op-Team/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Winter99Co_Op_Team.elasticsearch.SearchConnection.Search;
+
+namespace Winter99Co_Op_Team.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountSearcher _accountSearcher;
+
+        public AccountController(IAccountSearcher accountSearcher)
+        {
+            _accountSearcher = accountSearcher;
+        }
+
+        [HttpPost]
+        public IActionResult GetAccount([FromBody] string accountId)
+        {
+            var account = _accountSearcher.GetAccountById(accountId);
+            return Ok(account);
+        }
+    }
+}
---
+++ Winter99Co-Op-Team/Controllers/TransactionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Winter99Co_Op_Team.elasticsearch.SearchConnection.Search;
+
+namespace Winter99Co_Op_Team.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionSearcher _transactionSearcher;
+
+        public TransactionController(ITransactionSearcher transactionSearcher)
+        {
+            _transactionSearcher = transactionSearcher;
+        }
+
+        [HttpPost]
+        public IActionResult GetTransactionsOfAccount([FromBody] string accountId)
+        {
+            var transactions = _transactionSearcher.GetAllTransactions(accountId);
+            return Ok(transactions);
+        }
+    }
+}
